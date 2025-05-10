31ae5806025ae53aa5e7eff75fe20698ab2161cc
Patrick <patang1115@gmail.com>
2025-05-09 22:22:39 -0500

    re-do admin menu

--- admin/admin-menu.php
+++ admin/admin-menu.php
@@ -1,26 +1,26 @@
 <?php
 
-function popup_trigger_admin_menu() {
+function feedback_form_admin_menu() {
     add_menu_page(
         'Feedback',       
         'Feedback List',        
         'manage_options',                  
-        'notion-feedback-form-admin',        
-        'notion_feedback_form_admin_page', 
+        'feedback-form-admin',        
+        'feedback_form_admin_page', 
         'dashicons-bell',                  
         25                                
     );
 }
 
-function notion_feedback_form_admin_page() {
+function feedback_form_admin_page() {
     ?>
     <div class="wrap">
-        <h1>List of Feedback</h1>
+        <h1>Feedback List</h1>
         <p>This is where you could configure settings for your popup if you want.</p>
         <!-- You could add options, checkboxes, settings form here -->
     </div>
     <?php
 }
 
-add_action('admin_menu', 'notion_feedback_form_admin_page');
+add_action('admin_menu', 'feedback_form_admin_menu');
 
